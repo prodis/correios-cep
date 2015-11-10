7dbb019fd48773a2fc2def314e15cc8e8ee32ede
Prodis <prodis@gmail.com>
2015-11-10 15:08:15 -0200

    Simplify fixtures loading.

--- spec/support/fixture.rb
+++ spec/support/fixture.rb
@@ -1,19 +1,5 @@
 module Fixture
-  class << self
-    def load(name)
-      case name
-      when :address,
-           :address_with_complement,
-           :address_with_two_complements,
-           :address_not_found
-        read_file_for(name)
-      else
-        raise ArgumentError, "Fixture '#{name}' not found."
-      end
-    end
-
-    def read_file_for(filename)
-      File.open("#{File.expand_path("../../", __FILE__)}/fixtures/#{filename}.xml").read
-    end
+  def self.load(name)
+    File.open("#{File.expand_path('../../', __FILE__)}/fixtures/#{name}.xml").read
   end
 end
