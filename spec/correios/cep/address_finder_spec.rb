20aa1a6dc778a2eb67fe73e09123e2f86c8f3024
Prodis <prodis@gmail.com>
2015-11-10 18:33:40 -0200

    Improve AddressFinder specs.

--- spec/correios/cep/address_finder_spec.rb
+++ spec/correios/cep/address_finder_spec.rb
@@ -2,23 +2,23 @@ require 'spec_helper'
 
 describe Correios::CEP::AddressFinder do
   context 'when zipcode is valid' do
-    let(:cep) { '54250610' }
+    let(:zipcode) { '54250610' }
     let(:web_service_response) { '<end>Rua Fernando Amorim</end>' }
     let(:address) { { address: 'Rua Fernando Amorim' } }
     let(:web_service) { double(Correios::CEP::WebService) }
     let(:parser) { double(Correios::CEP::Parser) }
     let(:dependencies) { { web_service: web_service, parser: parser } }
 
-    subject { described_class.new(dependencies) }
-
     before do
-      allow(web_service).to receive(:request).with(cep).and_return(web_service_response)
+      allow(web_service).to receive(:request).with(zipcode).and_return(web_service_response)
       allow(parser).to receive(:hash).with(web_service_response).and_return(address)
     end
 
     describe '#get' do
+      subject { described_class.new(dependencies) }
+
       it 'returns address' do
-        expect(subject.get(cep)).to eq address
+        expect(subject.get(zipcode)).to eq address
       end
     end
 
@@ -26,25 +26,27 @@ describe Correios::CEP::AddressFinder do
       subject { described_class }
 
       it 'returns address' do
-        expect(subject.get(cep, dependencies)).to eq address
+        expect(subject.get(zipcode, dependencies)).to eq address
       end
     end
   end
 
-  {
-    'zipcode is required' => {
-      'is nil' => nil,
-      'is empty' => ''
-    },
-    'zipcode in invalid format (valid format: 00000-000)' => {
-      'has less than 8 digits' => '1234567',
-      'has invalid format' => '1234-5678'
-    }
-  }.each do |error_message, values|
-    values.each do |text, value|
-      context "when zipcode #{text}" do
-        it "raises ArgumentError with '#{error_message}' error message" do
-          expect { subject.get(value) }.to raise_error(ArgumentError, error_message)
+  context 'when zipcode is not valid' do
+    {
+      'zipcode is required' => {
+        'nil value' => nil,
+        'empty value' => ''
+      },
+      'zipcode in invalid format (valid format: 00000-000)' => {
+        'less than 8 digits' => '1234567',
+        'invalid format' => '1234-5678'
+      }
+    }.each do |error_message, values|
+      values.each do |text, value|
+        context "with #{text}" do
+          it "raises ArgumentError with '#{error_message}' error message" do
+            expect { subject.get(value) }.to raise_error(ArgumentError, error_message)
+          end
         end
       end
     end
