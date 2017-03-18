7af076fc6da8f902a05b1b6b38de168cdfa1071b
Prodis <prodis@gmail.com>
2017-03-18 14:44:07 +0100

    Frozen string literal directive.

--- lib/correios-cep.rb
+++ lib/correios-cep.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require 'correios/cep/config'
 require 'correios/cep/address_finder'
 require 'correios/cep/parser'
--- lib/correios/cep/version.rb
+++ lib/correios/cep/version.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 module Correios
   module CEP
     VERSION = '0.6.2'
