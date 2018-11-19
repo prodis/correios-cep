19f6af694cbcc564af25a02ec92eedf3814f3b17
Prodis <prodis@gmail.com>
2018-11-19 09:16:51 +0100

    Require version file in order to avoid uninitialised constant errors.

--- lib/correios-cep.rb
+++ lib/correios-cep.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require 'correios/cep/config'
+require 'correios/cep/version'
 require 'correios/cep/address_finder'
 require 'correios/cep/parser'
 require 'correios/cep/web_service'
